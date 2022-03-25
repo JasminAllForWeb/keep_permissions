5dc063b80ceecb62901ea7fc75aeb744a4255a63
Jasmin <contact@visbe.fr>
2022-03-25 12:31:37 +0100

    Fix script

--- keep_permissions.sh
+++ keep_permissions.sh
@@ -6,11 +6,21 @@ while true; do
 
 inotifywait -e modify,create,delete,move -r $1 |
 	while read -r path; do
-		#dir=$(git rev-parse --show-toplevel)
-		#username=$dir | regex1 '.+client_(.+)'
-		#chown -R www-data:$username $dir
-		echo $path >> ~/log_deploy
-		echo $username >> ~/log_deploy
+		if [[ $path =~ ((.+)client_(.+))\/( [A-Z]+ (.+)) ]]; then 
+			root_path=${BASH_REMATCH[1]}
+			username=${BASH_REMATCH[3]}
+			f=${BASH_REMATCH[5]}
+			full_path="$root_path/$f"
+  			echo "The full path is '$full_path'." >> ~/log_deploy
+  			echo "The root path is '$root_path'." >> ~/log_deploy
+  			echo "The folder/file is '$f'." >> ~/log_deploy
+  			echo "The user is '$username'." >> ~/log_deploy
+  			command="sudo chown -R www-data:$username $full_path"
+  			echo $command >> ~/log_deploy
+  			`$command`>> ~/log_deploy
+		else
+			echo "ERROR User not detected with '$path'" >> ~/log_deploy
+		fi
 	done
 done
 
